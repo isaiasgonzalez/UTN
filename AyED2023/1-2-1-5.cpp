c8632c4fdfebef9d0a12fde6371da7643775ccc5
Isaias Gonzalez <isgonzalez@frba.utn.edu.ar>
2023-04-15 23:35:46 -0300

    Mayor y menor valor entre dos numeros

---
+++ AyED2023/1-2-1-5.cpp
@@ -0,0 +1,33 @@
+// Se ingresan por teclado dos valores numericos enteros
+// Informar cual es el mayor y cual es el menor
+// Si son iguales, mostrar un mensaje con el siguiente texto:
+// " Los valores ingresados son iguales "
+
+#include <iostream>
+
+int main() {
+
+    // Se ingresan por teclado dos valores numericos enteros
+
+    int a;
+    int b;
+
+    std::cout << "Ingresar dos valores numericos enteros: " << std::endl;
+    std::cin >> a;
+    std::cin >> b;
+
+    // Informar cual es el mayor y cual es el menor
+
+    if ( a > b ) {
+
+        std::cout << a << " es mayor que " << b;
+
+    } else if ( b > a)
+    {
+        std::cout << b << " es mayor que " << a;
+    } else {
+        std::cout << "Los valores ingresados son iguales";
+    }
+    
+
+}
