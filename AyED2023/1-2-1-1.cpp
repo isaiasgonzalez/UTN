8ea50b31d1bcaa3c9da8a524414b6fcc5a25a121
Isaias Gonzalez <isgonzalez@frba.utn.edu.ar>
2023-04-15 22:05:03 -0300

    Operaciones aritmeticas

---
+++ AyED2023/1-2-1-1.cpp
@@ -0,0 +1,8 @@
+// Operaciones aritmeticas
+// Se ingresan por teclado dos valores numericos enteros
+// Se pide calcular e informar por consola
+// La suma a + b
+// La diferencia a - b
+// El producto a * b
+// El cociente a / b , aceptando que b es distinto de 0
+
