166ef8f7285d82e310631ae90d3992c2c2a4ae7b
Isaias Gonzalez <isgonzalez@frba.utn.edu.ar>
2023-04-15 22:13:22 -0300

    Operaciones aritmeticas

--- AyED2023/1-2-1-1.cpp
+++ AyED2023/1-2-1-1.cpp
@@ -6,3 +6,44 @@
 // El producto a * b
 // El cociente a / b , aceptando que b es distinto de 0
 
+#include <iostream>
+
+int main() {
+
+    // Se ingresan por teclado dos valores numericos enteros
+
+    int a;
+    int b;
+
+    std::cout << "Ingresar el primer valor: ";
+    std::cin >> a;
+
+    std::cout << "Ingresar el segundo valor: ";
+    std::cin >> b;
+
+    int resultado;
+
+    // La suma a + b
+    resultado = a + b;
+    std::cout << "El resultado de la suma es: " << resultado << std::endl;
+
+    // La diferencia a - b
+    resultado = a - b;
+    std::cout << "El resultado de la diferencia es: " << resultado << std::endl;
+
+    // El producto a * b
+    resultado = a * b;
+    std::cout << "El resultado del producto es: " << resultado << std::endl;
+
+    // El cociente a / b
+
+    if ( b != 0 )
+    {
+            resultado = a / b;
+            std::cout << "El cociente de a/b es " << resultado << std::endl;
+    } else {
+        std::cout << "El cociente no se puede realizar porque el valor de b es 0.";
+        }  
+    return 0;
+
+}
