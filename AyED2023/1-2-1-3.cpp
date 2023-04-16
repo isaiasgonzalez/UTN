3e04863f1c959cb6fc2dfff9f3a5b003e038caa8
Isaias Gonzalez <isgonzalez@frba.utn.edu.ar>
2023-04-15 22:57:13 -0300

    Division entera y modulo

---
+++ AyED2023/1-2-1-3.cpp
@@ -0,0 +1,32 @@
+// Se ingresa por teclado un valor numerico entero, informar
+// 1. La quinta parte de dicho valor
+// 2. El resto que surge de dividir el valor ingresado en cinco partes iguales
+// 3. La septima parte de la quinta parte del valor ingresado
+
+#include <iostream>
+
+int main() {
+
+    // Se ingresa por teclado un valor numerico entero
+    int n;
+    std::cout << "Ingrese un valor numerico entero: ";
+    std::cin >> n;
+
+    int resultado;
+
+    // 1. La quinta parte de dicho valor
+
+    resultado = n / 5;
+    std::cout << "La quinta parte de " << n << " es: " << resultado << std::endl;
+
+    // 2. El resto que surge de dividir el valor ingresado en cinco partes iguales
+
+    resultado = n % 5;
+    std::cout << "El resto que surge de dividir " << n << " en cinco partes iguales es: " << resultado << std::endl;
+
+    // 3. La septima parte de la quinta parte del valor ingresado
+
+    resultado = ( n / 5 ) / 7;
+    std::cout << "La septima parte de la quinta parte de " << n << " es: " << resultado << std::endl;
+
+}
